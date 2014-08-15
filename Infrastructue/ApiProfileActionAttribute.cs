ad9d7a7b77b9a0dcd3f9d62442d325f1c84a632c
unknown <donaldloo@SIN-ITNB04.euro.local>
2014-08-15 18:21:50 +0800

    added infrastructure.apiprofileaction

---
+++ Infrastructue/ApiProfileActionAttribute.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Data.Entity;
+using Ames.Entities;
+
+
+namespace Ames.Infrastructue {
+    public class ApiProfileActionAttribute : ActionFilterAttribute {
+        
+        private Stopwatch timer;
+        private EFAmesInfra db = new EFAmesInfra();
+        string moduleName;
+        string controllerName;
+        string actionName;
+        string machineName;
+        string absoluteURL;
+
+        private decimal actionDuration;
+//        private decimal resultDuration;
+
+
+
+        public override void OnActionExecuting(HttpActionContext filterContext) {
+            machineName = System.Web.HttpContext.Current.Request.UserHostName + " = " +
+                System.Web.HttpContext.Current.Request.UserHostAddress;  //filterContext.HttpContext.Server.MachineName;
+            absoluteURL = filterContext.Request.RequestUri.AbsoluteUri; //filterContext.HttpContext.Request.Url.AbsoluteUri;
+            timer = Stopwatch.StartNew();
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext filterContext) {
+            timer.Stop();
+            actionDuration = (decimal) timer.Elapsed.TotalMilliseconds;
+            moduleName = filterContext.ActionContext.ActionDescriptor.ControllerDescriptor.ControllerType.UnderlyingSystemType.Module.Name;  //filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.UnderlyingSystemType.Module.Name;
+            controllerName = (filterContext.ActionContext.ActionDescriptor).ControllerDescriptor.ControllerName; //filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            actionName = filterContext.Request.Method.Method + " - " + (filterContext.ActionContext.ActionDescriptor).ActionName; //filterContext.ActionDescriptor.ActionName;
+
+            
+                ProfileActionLog record = new ProfileActionLog {
+                    //ProfileID = 0,
+                    DateTime = System.DateTime.Now,
+                    UserName = filterContext.ActionContext.RequestContext.Principal.Identity.Name, // filterContext.RequestContext.HttpContext.User.Identity.Name,
+                    Module = moduleName,
+                    Controller = controllerName,
+                    Action = actionName,
+                    BrowserType = ((System.Web.Configuration.HttpCapabilitiesBase)(System.Web.HttpContext.Current.Request.Browser)).Browser, //filterContext.HttpContext.Request.Browser.Type,
+                    MachineName = machineName,
+                    AbsoluteURL = absoluteURL,
+                    ActionDuration = actionDuration,
+                    //ErrorMessage = filterContext.Exception.Message + ">>" + filterContext.Exception.StackTrace,
+                };
+                if (filterContext.Exception != null)
+                    record.ErrorMessage = filterContext.Exception.Message + ">>" + filterContext.Exception.StackTrace;
+                // save the profilelog into Db
+                db.ProfileActionLogs.Add(record);
+                db.SaveChanges();
+            
+        }
+
+        /*
+        public override void OnResultExecuting(ResultExecutingContext filterContext) {
+            timer.Restart();
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext) {
+            
+            timer.Stop();
+            string errorMessage = null;
+            
+            resultDuration = (decimal)timer.Elapsed.TotalMilliseconds;
+                
+            if (filterContext.Exception != null) {
+                errorMessage = filterContext.Exception.Message + ">>" + filterContext.Exception.StackTrace;
+            }
+            // save the profilelog into Db  
+            ProfileActionLog record = new ProfileActionLog {
+                //ProfileID = 0,
+                DateTime = System.DateTime.Now,
+                UserName = filterContext.RequestContext.HttpContext.User.Identity.Name,
+                Module = moduleName,
+                Controller = controllerName,
+                Action = actionName,
+                BrowserType = filterContext.HttpContext.Request.Browser.Type,
+                MachineName = machineName,
+                AbsoluteURL = absoluteURL,
+                ActionDuration = actionDuration,
+                ResultDuration = resultDuration,
+                ErrorMessage = errorMessage,
+            };
+            db.ProfileActionLogs.Add(record);
+            db.SaveChanges();
+        }
+        */
+
+    }
+}
---
+++ Tracing/AmesTraceWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Tracing;
+
+namespace Ames.Tracing {
+    public class AmesTraceWriter : ITraceWriter {
+
+
+        public void Trace(HttpRequestMessage request, string category,
+                          TraceLevel level, Action<TraceRecord> traceAction) {
+            TraceRecord traceRecord = new TraceRecord(request, category, level);
+            traceAction(traceRecord);
+            
+            ShowTrace(traceRecord);
+        }
+
+        private void ShowTrace(TraceRecord traceRecord) {
+            //Console.WriteLine(
+            var printTraceRecord = String.Format(
+                "{0} {1}: Category={2}, Level={3} {4} {5} {6} {7}",
+                traceRecord.Request.Method.ToString(),
+                traceRecord.Request.RequestUri.ToString(),
+                traceRecord.Category,
+                traceRecord.Level,
+                traceRecord.Kind,
+                traceRecord.Operator,
+                traceRecord.Operation,
+                traceRecord.Exception != null
+                    ? traceRecord.Exception.GetBaseException().Message
+                    : !string.IsNullOrEmpty(traceRecord.Message)
+                        ? traceRecord.Message
+                        : string.Empty
+             );
+              //  ));
+            
+            Console.WriteLine(printTraceRecord);
+
+            string path = HttpContext.Current.Server.MapPath("~/Logs/");
+            //Creates all directories and subdirectories in the specified path unless they already exist.
+            Directory.CreateDirectory(path);
+            File.AppendAllText(path + "MyTestLog.txt", printTraceRecord + "\r\n");
+        }
+
+    }
+}
